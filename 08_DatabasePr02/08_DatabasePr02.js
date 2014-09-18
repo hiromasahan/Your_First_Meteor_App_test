b4852218f60d017fb8a41d29cf5082266ab2e194
hiromasahan <hiromasahan@163.com>
2014-09-18 16:56:36 +0800

    I can push the translations of lsn 04 $ 05 eventually after the endless updating...TAT

--- 08_DatabasePr02/08_DatabasePr02.js
+++ 08_DatabasePr02/08_DatabasePr02.js
@@ -1,5 +1,9 @@
 if (Meteor.isClient) {
   
+  Template.leaderboard.player = function(){
+    return PlayersList.find({});
+  }
+  
   Template.leaderboard.events({
     'click li.player': function () {
       var playerId = this.id;
@@ -10,6 +14,12 @@ if (Meteor.isClient) {
 
     'click #increment': function(){
       var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update(
+        {_id: selectedPlayer},
+        {$set:{score : 5}}
+        //{$inc:{score:5}} can also fulfill this
+        );
+      }
     }
   });
 }
